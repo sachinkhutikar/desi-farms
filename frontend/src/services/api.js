4b968fcc6fc02c6a7cb3671533c07f0c2ecd80a7
Sachin Khutikar <sachinkhutikar2@gmail.com>
2026-03-02 15:43:30 +0530

    get error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + "/api",
+  /*baseURL: process.env.REACT_APP_API_URL + "/api",*/
   headers: {
     "Content-Type": "application/json",
   },
